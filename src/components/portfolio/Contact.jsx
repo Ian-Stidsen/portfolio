a46ca432ffcb4213f1f9380cb8a177a586955d7d
Ian Stidsen <ianlundstidsen@gmail.com>
2023-02-04 18:54:51 -0300

    emailjs

--- src/components/portfolio/Contact.jsx
+++ src/components/portfolio/Contact.jsx
@@ -1,9 +1,33 @@
 import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser'
+
 import './stylesheet/Contact.css';
 
 export function Contact() {
 
   const emailForm = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    /* "portfolio-service",
+    "portfolio-template",
+    emailForm.current,
+    "8nesMxDbdPp8ZsJoH" */
+    emailjs.sendForm(
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
+      emailForm.current,
+      process.env.REACT_APP_API_KEY
+    ).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    )
+  };
 
   return (
     <>
@@ -13,7 +37,7 @@ export function Contact() {
       <input type="text" id='subject' name='subject' placeholder='Subject' />
       <textarea id='message' name='message' placeholder='Message'></textarea>
     </form>
-    <button className='submit-contact'>Submit</button>
+    <button className='submit-contact' onClick={sendEmail}>Submit</button>
     </>
   )
 }
